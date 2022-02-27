bdfea713d20d18dd3c065814ab18c61314df9c2a
novitskynick <novitskynick@gmail.com>
2022-02-27 18:04:30 +0300

    Initial commit

---
+++ src/test/java/ru/gb/mongo_example/MongoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.mongo_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
